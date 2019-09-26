9672af9f77a8fa13626464124aa86e1911534172
kanup4m <kanupam500@gmail.com>
2019-09-26 20:40:20 +0530

    Updates

---
+++ precache-manifest.beef994bbc62cf7ac0e7fc9264bbce24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89429be0a7494d15a7f5b32e1020515f",
+    "url": "/Coin-Flipper/index.html"
+  },
+  {
+    "revision": "30c59451e85ef3b94b59",
+    "url": "/Coin-Flipper/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3839cc2b0bd66ec78a61",
+    "url": "/Coin-Flipper/static/js/2.f792796b.chunk.js"
+  },
+  {
+    "revision": "30c59451e85ef3b94b59",
+    "url": "/Coin-Flipper/static/js/main.b8df5234.chunk.js"
+  },
+  {
+    "revision": "b0ea581017b8968d18c7",
+    "url": "/Coin-Flipper/static/js/runtime-main.aee6f3df.js"
+  }
+]);
